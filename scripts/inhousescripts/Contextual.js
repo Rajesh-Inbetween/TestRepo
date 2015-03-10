06da337e2628abc9396d24e319b6a1cd67c98449
Rohith Premachandran <rohith@inbetween.com>
2015-03-10 23:05:02 +0530

    populate grids api in the making

--- scripts/inhousescripts/Contextual.js
+++ scripts/inhousescripts/Contextual.js
@@ -41,12 +41,48 @@ function getContentById(id){
 
 function populateGrids(){
 
-  var aContent = $.extend(false, [], aContentData);
-
+  var aContent = $.extend(true, [], aContentData);
   var oUserData = sessionStorage.userData;
+  var oRuleData = $.extend(true,{},oRules);
+  for(sTargetGroupType in oRuleData.targetGroup){
+    if(oRuleData.targetGroup[sTargetGroupType] > 0){
+      var oContent = getContentWithTargetGroup(aContent, sTargetGroupType);
+      if(oContent){
+        oRuleData.targetGroup[sTargetGroupType]--;
+        var aRegions = oContent["Region"];
+        for(var iRegionIndex = 0 ; iRegionIndex < aRegions.length ; iRegionIndex++){
+          var sRegionName = aRegions[iRegionIndex].name;
+          if(oRuleData.region[sRegionName] > 0){
+            oRuleData.region[sRegionName]--;
+          }
+        }
+      }
+    }
+  }
+  console.log(oRuleData);
 
-  var oRuleData = oRules;
+}
 
+/**
+ *
+ * @param aClonedContent - Content that has been Cloned!!!
+ * @param sTargetGroup
+ */
+function getContentWithTargetGroup(aClonedContent, sTargetGroup){
+  for(var iContentIndex = 0 ; iContentIndex < aClonedContent.length ; iContentIndex++){
+    var oContent = aClonedContent[iContentIndex];
+    var aContentTargetGroups = oContent["Target Group"];
+    for(var iTargetGroupIndex=0 ; iTargetGroupIndex < aContentTargetGroups.length ; iTargetGroupIndex++){
+      var oTargetGroup = aContentTargetGroups[iTargetGroupIndex];
+      if(oTargetGroup.name == sTargetGroup && oTargetGroup.relevance > 0){
+        aClonedContent.splice(iContentIndex,1);
+        return oContent;
+      }
+    }
+  }
+  return false;
+}
 
+function getContentForRegion(aContent, sRegion){
 
 }
--- tacks/Rules.js
+++ tacks/Rules.js
@@ -1,12 +1,16 @@
 var oRules = {
-  "Financial Affinity":1,
-  "Internationality":2,
-  "Digital Affinity":1,
-  "Socioeconomic Group":0,
-  "Family Status":1,
-  "Cultural Type":0,
-  "North":2,
-  "South":0,
-  "East":0,
-  "West":1
+  "targetGroup":{
+    "Financial Affinity":1,
+    "Internationality":1,
+    "Digital Affinity":1,
+    "Socioeconomic Group":0,
+    "Family Status":0,
+    "Cultural Type":0
+  },
+  "region":{
+    "North":2,
+    "South":0,
+    "East":0,
+    "West":1
+  }
 };
