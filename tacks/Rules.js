60fa1f572aa4263600546c6a9988a8fbaea25697
Rohith Premachandran <rohith@inbetween.com>
2015-03-10 22:26:44 +0530

    added regions to rules

--- tacks/Rules.js
+++ tacks/Rules.js
@@ -4,5 +4,9 @@ var oRules = {
   "Digital Affinity":1,
   "Socioeconomic Group":0,
   "Family Status":1,
-  "Cultural Type":0
+  "Cultural Type":0,
+  "North":2,
+  "South":0,
+  "East":0,
+  "West":1
 };
