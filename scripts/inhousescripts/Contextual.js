153338e4f01a52922e96bed0d72ec23242aea436
Rohith Premachandran <rohith@inbetween.com>
2015-03-10 21:13:58 +0530

    removed SessionStorage handler

--- scripts/inhousescripts/Contextual.js
+++ scripts/inhousescripts/Contextual.js
@@ -43,7 +43,7 @@ function populateGrids(){
 
   var aContent = aContentData;
 
-  var oUserData = oDefaultUserSessionData;
+  var oUserData = sessionStorage.userData;
 
   
 
--- scripts/inhousescripts/SessionStorageHandler.js
+++ scripts/inhousescripts/SessionStorageHandler.js
@@ -13,35 +13,3 @@ sessionData = {
 $(window).unload(function () {
   sessionStorage.userData = JSON.stringify(sessionData.userData);
 });
-
-function SessionStorageHandler () {
-
-  var oSessionStorage = sessionData;
-
-  this.getSessionData = function () {
-
-    return oSessionStorage;
-  }
-
-  this.setDataToSession = function (sKey, oData) {
-    oSessionStorage[sKey] = oData;
-  }
-
-  this.getDataFromSession = function (sKey) {
-
-    return oSessionStorage[sKey];
-  }
-
-}
-
-
-SessionStorageHandler._instance = null;
-
-SessionStorageHandler.getInstance = function () {
-
-  if (!SessionStorageHandler._instance) {
-    SessionStorageHandler._instance = new SessionStorageHandler();
-  }
-
-  return SessionStorageHandler._instance;
-}
