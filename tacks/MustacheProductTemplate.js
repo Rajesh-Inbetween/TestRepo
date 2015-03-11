dae70d7ca7be89313447bcf6f42294ed714ff663
aayush <aayush@inbetween.co.in>
2015-03-12 01:33:10 +0530

    css is changed dor the grid cells

--- tacks/MustacheProductTemplate.js
+++ tacks/MustacheProductTemplate.js
@@ -1,39 +1,45 @@
 var sMustacheSmallTemplate =
     "<div class='product_template_data'>"+
 
-/*
-    "<div class='product_template_label'>{{label}}</div>" +
-    "<div class='product_template_content'>{{content}}</div>" +
-*/
 
-    "<div class='product_template_container'>"+
-    "<div class='small_product-data-details'>" +
-    "<div class='small_product_template_label_info'>{{label}}</div>" +
-    "<div class='small_product_template_content_info'>{{content}}</div>" +
-    "</div>"+
+    //"<div class='product_template_label'>{{label}}</div>" +
+    //"<div class='product_template_content'>{{content}}</div>" +
 
-    "<div class='product_template small_product_template' data-id='{{id}}' data-content='{{content}}' style='background-image: url(\"{{image}}\")'>" +
-    "<div class='product_template_description'>{{description}}</div> " +
-    "</div>" +
-    "</div>" +
+      "<div class='product_template_container'>"+
+
+        //"<div class='small_product-data-details'>" +
+        "<div class='product-label small_product_template_label_info'>{{label}}</div>" +
+        //"</div>"+
+
+        "<div class='product_data_image'>"+
+          "<div class='product_template small_product_template' data-id='{{id}}' data-content='{{content}}' style='background-image: url(\"{{image}}\")'></div>" +
+          //"<div class='product_template_description'>{{description}}</div> " +
+        "</div>" +
+
+        "<div class='product-content small_product_template_content_info'>{{content}}</div>" +
+
+      "</div>" +
+
     "</div>";
 
 var sMustacheLargeTemplate =
     "<div class='product_template_data'>"+
 
-  /*  "<div class='product_template_label'>{{label}}</div>" +
-    "<div class='product_template_content'>{{content}}</div>" +*/
+    //"<div class='product_template_label'>{{label}}</div>" +
+    //"<div class='product_template_content'>{{content}}</div>" +
 
-    "<div class='product_template_container'>"+
-    "<div class='product-data-details'>" +
-    "<div class='large_product_template_label_info'>{{label}}</div>" +
-    "<div class='large_product_template_content_info'>{{content}}</div>" +
-    "</div>"+
+      "<div class='product_template_container'>"+
 
-    "<div class='product_data_image'>"+
-    "<div class='product_template' data-id='{{id}}' data-content='{{content}}' style='background-image: url(\"{{image}}\")'>" +
-    "<div class='product_template_description'>{{description}}</div> " +
-    "</div>" +
-    "</div>"+
-    "</div>"+
+        "<div class='product-data-details'>" +
+          "<div class='product-label large_product_template_label_info'>{{label}}</div>" +
+          "<div class='product-content large_product_template_content_info'>{{content}}</div>" +
+        "</div>"+
+
+        "<div class='product_data_image'>"+
+          "<div class='product_template large_product_template' data-id='{{id}}' data-content='{{content}}' style='background-image: url(\"{{image}}\")'></div>" +
+          //"<div class='product_template_description'>{{description}}</div> " +
+        "</div>" +
+
+      "</div>"+
+
     "</div>";
