7c9b01fe11232e1a392057e9e9eaeefdf013076e
aayush <aayush@inbetween.co.in>
2015-03-11 16:05:57 +0530

    show dialog on cell clicked

--- scripts/inhousescripts/createGrid.js
+++ scripts/inhousescripts/createGrid.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   //getMustacheTemplateDom(oData);
   //addProductDetailsToCells(aContentData);
   //addProductDetailsToCell(0,1,aContentData);
+  $('.grid .grid-row .grid-cell').on('click',cellClicked);
   populateGrids();
 });
 
@@ -15,6 +16,7 @@ addProductDetailsToCell = function(rowCount, cellCount, oData){
 addProductDetailsToCells = function (aData) {
   for (var i = 0; i < aData.length; i++) {
     var output = Mustache.to_html(sMustacheTemplate, aData[i]);
+    $(".grid .grid-row .grid-cell").eq(i).attr("data-content","content");
     $(".grid .grid-row .grid-cell").eq(i).html(output);
   }
 
@@ -22,4 +24,17 @@ addProductDetailsToCells = function (aData) {
 
 getMustacheTemplateDom = function(oData){
   return  Mustache.to_html(sMustacheTemplate, oData);
+}
+
+cellClicked = function(oEvent){
+  var oTarget = oEvent.target;
+  if(oTarget.className == "grid-cell"){
+    showDialog($(oTarget), oTarget.getAttribute("data-content"));
+  }
+}
+
+function showDialog ( oTarget, dataContentt) {
+  $('#dialog').html(dataContentt);
+  $('#dialog').dialog({closeText: "",
+                      position : { of : oTarget}});
 }
